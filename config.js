8d81eaaaf7839358fb077319fc16bd985a9d24cd
wemomax <wemomax@gmail.com>
2022-07-06 00:57:41 -0300

    squido

---
+++ config.js
@@ -0,0 +1,48 @@
+const config = {
+    development: {
+        name: 'DDS',
+        description: 'Desenvolvimento Distribuído de Software',
+        twitterHandle: '@welington',
+        baseUrl: 'http://localhost:4965',
+        sourcesExt: 'markdown',
+        summaryLength: 250,
+        port: 4965,
+        pagination: true,
+        postPerPage: 8,
+        plugins: [
+            {
+                name: 'search',
+                options: {}
+            }
+        ],
+        postBuild: [
+            {
+                name: 'zip',
+                options: {}
+            }
+        ]
+    },
+    production: {
+        name: 'DDS',
+        description: 'Desenvolvimento Distribuído de Software',
+        twitterHandle: '@welington',
+        baseUrl: 'https://wemomax.github.io/squido',
+        sourcesExt: 'markdown',
+        summaryLength: 250,
+        port: 4965,
+        pagination: true,
+        postPerPage: 8,
+        plugins: [
+            {
+                name: 'search',
+                options: {}
+            },
+            {
+                name: 'robots',
+                options: {}
+            }
+        ]
+    }
+};
+
+module.exports = config;
